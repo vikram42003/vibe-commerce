3f2e24fe82d05d63b54cd1bc17bb4791e77c8162
vikram(wsl) <vikramjit360@gmail.com>
2025-10-28 22:50:54 +0530

    feat: initialize react query, react router, axios

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -3,15 +3,17 @@ import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import { defineConfig, globalIgnores } from "eslint/config";
+import pluginQuery from "@tanstack/eslint-plugin-query";
 
 export default defineConfig([
+  ...pluginQuery.configs["flat/recommended"],
   globalIgnores(["dist"]),
   {
     files: ["**/*.{js,jsx}"],
     extends: [js.configs.recommended, reactHooks.configs["recommended-latest"], reactRefresh.configs.vite],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: { ...globals.browser, ...globals.node},
+      globals: { ...globals.browser, ...globals.node },
       parserOptions: {
         ecmaVersion: "latest",
         ecmaFeatures: { jsx: true },
